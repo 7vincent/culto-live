fa198f07343fc44e86998c9437c69cad458d77a9
Vicente Albuquerque <7.vincent@gmail.com>
2020-05-17 23:11:04 -0300

    Insert pages

--- src/pages/Aovivo/index.js
+++ src/pages/Aovivo/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-// import { Image }  from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { Form } from '@rocketseat/unform';
-import * as Yup from 'yup';
-import { useDispatch } from 'react-redux';
-import TextField from '~/components/Material/CustomTextField';
-import { signInRequest } from '~/store/modules/auth/actions';
-import Logo from '~/components/Logo';
 
 //import logoAlgo from '~/assets/algo.png';
 
@@ -50,47 +40,19 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const schema = Yup.object().shape({
-  email: Yup.string()
-    .email('Insira um e-mail válido')
-    .required('O e-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória'),
-});
 
 export default function SignIn() {
-  const dispatch = useDispatch();
-  const classes = useStyles();
-  const ano = new Date().getFullYear();
 
-  function handleSubmit({ email, password }) {
-    dispatch(signInRequest(email, password));
-  }
+  const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Logo width={120} />
-        <Form schema={schema} onSubmit={handleSubmit} className={classes.form}>
-          <TextField label="Email" name="email" type="email" />
-          <TextField label="Password" name="password" type="password" />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Entrar
-          </Button>
-        </Form>
-      </Paper>
       <Typography
         variant="subtitle2"
         align="center"
         className={classes.powered}
       >
-        <img src="" alt="" /> Algo Tecnologia | {ano}
+        Ao vivo
       </Typography>
     </Container>
   );
