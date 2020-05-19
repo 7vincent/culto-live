699da14a2abb420be9be671cf462e91420742f04
Vicente Albuquerque <7.vincent@gmail.com>
2020-05-19 00:07:00 -0300

    insert favicon e analytics

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-167046032-1');
+
 ReactDOM.render(<App />, document.getElementById('root'));
